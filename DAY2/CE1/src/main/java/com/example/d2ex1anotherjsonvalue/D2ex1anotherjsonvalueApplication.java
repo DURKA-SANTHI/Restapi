7245d3edb7c87242b2e0421b9c107bb2f8bda76d
DURKA-SANTHI <124854878+DURKA-SANTHI@users.noreply.github.com>
2024-03-04 09:27:51 +0530

    Day 2

---
+++ DAY2/CE1/src/main/java/com/example/d2ex1anotherjsonvalue/D2ex1anotherjsonvalueApplication.java
@@ -0,0 +1,13 @@
+package com.example.d2ex1anotherjsonvalue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D2ex1anotherjsonvalueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D2ex1anotherjsonvalueApplication.class, args);
+	}
+
+}
---
+++ DAY2/CE1/src/test/java/com/example/d2ex1anotherjsonvalue/D2ex1anotherjsonvalueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.d2ex1anotherjsonvalue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D2ex1anotherjsonvalueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
